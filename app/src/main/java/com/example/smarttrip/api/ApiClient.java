9c74d935847306fb08f3c1afb26b35564a153a05
AminMezouer <amezouer@gmail.com>
2026-05-20 15:41:39 +0200

    amelioration

--- app/src/main/java/com/example/smarttrip/api/ApiClient.java
+++ app/src/main/java/com/example/smarttrip/api/ApiClient.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    private static final String BASE_URL = "http://192.168.1.116:8000/";
+    private static final String BASE_URL = "https://travel-tracker-backend-j5q0/onrende.com/";
 
     private static ApiClient instance;
     private ApiService apiService;
